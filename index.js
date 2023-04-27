0e82dc1db741a39e932f3335a612726e339704af
Aravind <aravindben562@gmail.com>
2023-04-27 14:32:19 +0530

    Second

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static(__dirname + "/public"));
 
-mongoose.connect("mongodb://0.0.0.0:27017/surveyUsersDB");
+mongoose.connect("mongodb+srv://aravindben562:7iNigIeUalRlyTSG@cluster0.ayhb0mp.mongodb.net/?retryWrites=true&w=majority&wtimeoutMS=5000/surveyUsersDB");
 
 const userSchema = new mongoose.Schema({
   First_Name : {
@@ -118,11 +118,35 @@ const userSchema = new mongoose.Schema({
   }
 });
 
+const govSurveySchema = new mongoose.Schema({
+  How_do_you_rate_the_overall_performance_of_the_current_government_in_India :String,
+  Do_you_believe_that_the_Indian_government_is_making_significant_progress_in_terms_of_economic_growth : String,
+  Are_you_satisfied_with_the_current_government_handling_of_the_COVID19_pandemic_in_India : String,
+  How_effective_do_you_think_the_Indian_government_efforts_are_in_tackling_corruption_in_the_country : String,
+  How_would_you_rate_the_Indian_government_efforts_in_improving_the_education_system_in_the_country : String
+});
+
+const eduSurveySchema = new mongoose.Schema({
+  How_familiar_are_you_with_the_new_education_policy : String,
+  What_aspects_of_the_new_education_policy_do_you_think_will_have_the_greatest_impact : String,
+  Do_you_believe_the_new_education_policy_adequately_addresses_the_needs_of_students_from_marginalized_communities : String,
+  What_do_you_think_about_the_increased_emphasis_on_technology_and_digital_learning_in_the_new_education_policy : String,
+  Overall_do_you_think_the_new_education_policy_is_a_step_in_the_right_direction_for_our_country_s_education_system : String
+});
+
 const user = mongoose.model("User",userSchema);
+
+const govSurvey = mongoose.model("govSurvey",govSurveySchema);
+const eduSurvey = mongoose.model("eduSurvey",eduSurveySchema);
 
 app.route("/")
 
+.get(function(req,res){
+  res.send("Hi");
+})
+
 .post(async function(req,res){
+  console.log(req.body);
   const fname = req.body.fname;
   const lname = req.body.lname;
   const email = req.body.email;
@@ -185,10 +209,205 @@ app.route("/")
 
 });
 
-app.route("/productSurvey")
+app.route("/GovSurvey")
 
 .post(async function(req,res){
-  console.log(req.body);
+  const q1 = req.body.question1;
+  let a1 = "";
+  switch (q1) {
+    case "Item 1":
+    a1="Excellent";
+    break;
+    case "Item 2":
+    a1="Good";
+    break;
+    case "Item 3":
+    a1="Average";
+    break;
+    case "Item 4":
+    a1="Poor";
+    break;
+    case "Item 5":
+    a1="Very Poor";
+    break;
+  }
+  const q2 = req.body.question2;
+  let a2 = "";
+  switch (q1) {
+    case "Item 1":
+    a2="Yes, I think the government is making significant progress";
+    break;
+    case "Item 2":
+    a2="No, I don't think the government is making significant progress";
+    break;
+    case "Item 3":
+    a2="I am not sure";
+    break;
+  }
+  const q3 = req.body.question3;
+  let a3 ="";
+  switch (q3) {
+    case "Item 1":
+    a3="Yes, I am satisfied";
+    break;
+    case "Item 2":
+    a3="No, I am not satisfied";
+    break;
+    case "Item 3":
+    a3="I am not sure";
+    break;
+  }
+  const q4 = req.body.question4;
+  let a4 = "";
+  switch (q4) {
+    case "Item 1":
+    a4="Very effective";
+    break;
+    case "Item 2":
+    a4="Somewhat effective";
+    break;
+    case "Item 3":
+    a4="Not very effective";
+    break;
+    case "Item 4":
+    a4="Not at all effective";
+    break;
+  }
+  const q5 = req.body.question5;
+  let a5 = "";
+  switch (q5) {
+    case "Item 1":
+    a5="Excellent";
+    break;
+    case "Item 2":
+    a5="Good";
+    break;
+    case "Item 3":
+    a5="Average";
+    break;
+    case "Item 4":
+    a5="Poor";
+    break;
+    case "Item 5":
+    a5="Very Poor";
+    break;
+  }
+  console.log(a1);
+  const data = new govSurvey({
+    How_do_you_rate_the_overall_performance_of_the_current_government_in_India :a1,
+    Do_you_believe_that_the_Indian_government_is_making_significant_progress_in_terms_of_economic_growth : a2,
+    Are_you_satisfied_with_the_current_government_handling_of_the_COVID19_pandemic_in_India : a3,
+    How_effective_do_you_think_the_Indian_government_efforts_are_in_tackling_corruption_in_the_country : a4,
+    How_would_you_rate_the_Indian_government_efforts_in_improving_the_education_system_in_the_country : a5
+  });
+
+  try{
+    await data.save();
+    res.status(200).send("Recorded Successfully");
+  }
+  catch(err){
+    res.status(404).send(err);
+  }
+});
+
+app.route("/EduSurvey")
+
+.post(async function(req,res){
+  const q1 = req.body.question1;
+  let a1 = "";
+  switch (q1) {
+    case "Item 1":
+    a1="Very familiar";
+    break;
+    case "Item 2":
+    a1="Somewhat familiar";
+    break;
+    case "Item 3":
+    a1="Not at all familiar";
+    break;
+  }
+  const q2 = req.body.question2;
+  let a2 = "";
+  switch (q2) {
+    case "Item 1":
+    a2="Vocational training and skill development";
+    break;
+    case "Item 2":
+    a2="Board exams and assessment systems";
+    break;
+    case "Item 3":
+    a2="Technology and digital learning";
+    break;
+    case "Item 4":
+    a2="Curriculum and pedagogy";
+    break;
+    case "Item 5":
+    a2="Teacher training and development";
+    break;
+  }
+  const q3 = req.body.question3;
+  let a3 ="";
+  switch (q3) {
+    case "Item 1":
+    a3="Yes, it is inclusive and equitable";
+    break;
+    case "Item 2":
+    a3="It needs more focus on addressing the specific needs of marginalized communities";
+    break;
+    case "Item 3":
+    a3="No, it does not address the needs of marginalized communities";
+    break;
+    case "Item 4":
+    a3="Not sure";
+    break;
+  }
+  const q4 = req.body.question4;
+  let a4 = "";
+  switch (q4) {
+    case "Item 1":
+    a4="It is a necessary change in the current times";
+    break;
+    case "Item 2":
+    a4="It will increase accessibility and inclusivity";
+    break;
+    case "Item 3":
+    a4="It may lead to a digital divide among students";
+    break;
+    case "Item 4":
+    a4="Not sure";
+    break;
+  }
+  const q5 = req.body.question5;
+  let a5 = "";
+  switch (q5) {
+    case "Item 1":
+    a5=" Yes, it is a much-needed reform";
+    break;
+    case "Item 2":
+    a5="It has some good ideas, but implementation will be key to its success";
+    break;
+    case "Item 3":
+    a5="No, it does not address the fundamental issues in our education system";
+    break;
+    case "Item 4":
+    a5=" Not sure";
+    break;
+  }
+  const data = new eduSurvey({
+    How_familiar_are_you_with_the_new_education_policy : a1,
+    What_aspects_of_the_new_education_policy_do_you_think_will_have_the_greatest_impact : a2,
+    Do_you_believe_the_new_education_policy_adequately_addresses_the_needs_of_students_from_marginalized_communities : a3,
+    What_do_you_think_about_the_increased_emphasis_on_technology_and_digital_learning_in_the_new_education_policy : a4,
+    Overall_do_you_think_the_new_education_policy_is_a_step_in_the_right_direction_for_our_country_s_education_system : a5
+  });
+
+  try{
+    await data.save();
+    res.status(200).send("Recorded Successfully");
+  }
+  catch(err){
+    res.status(404).send(err);
+  }
 });
 
 app.listen(3000 || process.env.PORT, () => {
